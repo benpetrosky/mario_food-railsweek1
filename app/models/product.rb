6c933c7e9a8d2a907751338d2f9be270593309af
Ben Petrosky <benpetrosky@hotmail.com>
2017-06-30 09:45:17 -0700

    initial commit. initial database set up adding CRUD to product object.

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+
+class Product < ActiveRecord::Base
+ has_many :reviews
+
+ validates :name, :presence => true
+ validates :cost, :presence => true
+ validates :country_of_origin, :presence => true
+end
