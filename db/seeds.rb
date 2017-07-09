583a9e82a1311d5f82e0d6dce850009447dc3452
Ben Petrosky <benpetrosky@hotmail.com>
2017-07-09 12:22:03 -0700

    add more styles to landing page

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ Review.destroy_all
                           {author: Faker::Name.unique.name,
                           content_body: Faker::Lorem.characters(100),
                           rating: Faker::Number.between(1, 5)},
-                          
+
                           {author: Faker::Name.unique.name,
                           content_body: Faker::Lorem.characters(100),
                           rating: Faker::Number.between(1, 5)}
@@ -33,11 +33,20 @@ Review.destroy_all
 
 end
 
-p "Created #{Product.count} products (coffee products)"
+10.times do |index|
+  Product.create!(name: Faker::Food.spice,
+                        country_of_origin: "United States",
+                        cost: Faker::Number.decimal(2),
+                        :reviews_attributes =>
+                        [
+                        {author: Faker::Name.unique.name,
+                        content_body: Faker::Lorem.characters(100),
+                        rating: Faker::Number.between(1, 5)},
 
-# 250.times do |index|
-# Review.create!(author: Faker::Name.unique.name,
-#                       content_body: Faker::Lorem.characters(100),
-#                       rating: Faker::Number.between(1, 5),
-#                       product_id: Faker::Number.between(65, 114))
-# end
+                        {author: Faker::Name.unique.name,
+                        content_body: Faker::Lorem.characters(100),
+                        rating: Faker::Number.between(1, 5)}
+                        ])
+end
+
+p "Created #{Product.count} products (coffee products)"
